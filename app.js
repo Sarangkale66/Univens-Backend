be2113985aeeead9e156a8dc67e3b891e7dcf477
pratiksha_B <pratikshabhokare283@gmail.com>
2025-01-27 12:43:35 +0530

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
+const cors=require('cors');
 const express=require('express');
 const app=express();
+app.use(cors());
  
 
 const connectDB = require('./config/mongdb.connection')
