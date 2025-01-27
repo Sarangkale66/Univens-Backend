a6a9141507bc4823d02ce8e7baee87417d1a54a4
pratiksha_B <pratikshabhokare283@gmail.com>
2025-01-27 12:39:32 +0530

    backend connectivity to react

---
+++ config/NodeMailer.config.js
@@ -0,0 +1,36 @@
+const nodeMailer = require('nodemailer');
+
+const html = `
+  <h1>Your Response has been Recorded</h1>
+  <p>Have a nice day 👋</p>
+  <img src="${process.env.SRC}" width="400">
+  `; 
+
+const mail=async(sender)=>{
+  try{
+    const transporter = nodeMailer.createTransport({
+      service:'gmail',
+      secure:true,
+      port:465,
+      auth:{
+        user:process.env.FROM,
+        pass:process.env.PASS
+      }
+    });
+
+    const info = await transporter.sendMail({
+      from:process.env.FROM,
+      to:sender,
+      subject:'Thank You For Visiting Univens',
+      html: html, 
+    });
+    return true;
+  }catch(err){
+    console.log("Message Error",err);
+    return false;
+  }
+  
+}
+
+module.exports = mail;
+
